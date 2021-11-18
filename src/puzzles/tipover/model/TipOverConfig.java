c2c74399e5dcd0c6ba1df0d9e8bfc9050332a6da
Damon Gonzalez <dbg3220@rit.edu>
2021-11-17 22:00:43 -0500

    Implemented the start constructor and toString() method within TipOverConfig.java

--- src/puzzles/tipover/model/TipOverConfig.java
+++ src/puzzles/tipover/model/TipOverConfig.java
@@ -13,7 +13,11 @@ import java.util.List;
  */
 public class TipOverConfig implements Configuration{
     /** The grid of squares that holds the value of the configuration */
-    private char[][] grid;
+    private char[] grid;
+    /** The amount of rows */
+    private int rows;
+    /** The amount of columns in each row */
+    private int cols;
     /** The location of the goal crate, does not change */
     private static Coordinates goal;
     /** The location of the tipper of this config */
@@ -25,18 +29,25 @@ public class TipOverConfig implements Configuration{
      * @param filename The name of the file that holds the puzzle
      */
     public TipOverConfig(String filename) throws FileNotFoundException {
-        BufferedReader in = new BufferedReader(new FileReader(filename));
-        try{
-            String line;
-            while(!(line = in.readLine()).equals("")){
-                System.out.println("Line of the File: " + line);
+        try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
+            String line = in.readLine();//ANALYZE FIRST LINE VALUES
+            String[] values = line.split("\\s++");
+            rows = Integer.parseInt(values[0]);
+            cols = Integer.parseInt(values[1]);
+            grid = new char[rows * cols];
+            tipperLocation = new Coordinates(Integer.parseInt(values[2]), Integer.parseInt(values[3]));
+            goal = new Coordinates(Integer.parseInt(values[4]), Integer.parseInt(values[5]));
+            int index = 0;
+            while(!(line = in.readLine()).equals("")){//ANALYZE THE GRID
+                String[] row = line.split("\\s++");
+                for(String square : row) {
+                    grid[index] = square.charAt(0);
+                    index++;
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        goal = new Coordinates(0, 0);//STUB VALUES
-        tipperLocation = new Coordinates(0, 0);//STUB VALUES
-        grid = new char[0][0];
     }
 
     /**
@@ -57,13 +68,57 @@ public class TipOverConfig implements Configuration{
         return null;
     }
 
-    @Override
-    public String getData() {
-        return null;
-    }
-
+    /**
+     * Represents this configuration as text symbols
+     * @return String representation
+     */
     @Override
     public String toString(){
-        return "";
+        String message = "   ";
+        for(int i = 0; i < cols; i++)
+            message += "  " + i;
+        message += "\n   ";
+        for(int i = 0; i < cols; i++)
+            message += "___";
+        message += "\n";
+        int start = 0;
+        for(int i = 0; i < rows; i++){
+            message += i + " |";
+            for(int j = start; j < start + cols; j++){
+                Coordinates c = new Coordinates(i, j);
+                message += " ";
+                if(goal.equals(c))
+                    message += "!";
+                else if(tipperLocation.equals(c))
+                    message += "*";
+                else
+                    message += " ";
+                message += get(c);
+            }
+            message += "\n";
+        }
+        return message;
+    }
+
+    /**
+     * Private helper function to abstract configuration's relationship to the array of characters representing
+     *  a two-dimensional grid. Sets a given location in grid to a specified new character
+     * @param a The Coordinate object that represents its row, col location
+     * @param c The char being set
+     */
+    private void set(Coordinates a, char c){
+        int index = a.row() * cols + a.col();
+        grid[index] = c;
+    }
+
+    /**
+     * Private helper function to abstract configuration's relationship to the array of characters representing
+     *  a two-dimensional grid. Returns a char at a given location in grid
+     * @param a The Coordinate object that represents its row, col location
+     * @return The char being gotten
+     */
+    private char get(Coordinates a){
+        int index = a.row() * cols + a.col();
+        return grid[index];
     }
 }
