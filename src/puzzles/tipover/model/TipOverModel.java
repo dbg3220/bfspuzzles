74549a6b5bf354532c84d0956813d3960444e699
Damon Gonzalez <dbg3220@rit.edu>
2021-11-20 23:58:49 -0500

    Fully implemented the model so it can be used by any UI

--- src/puzzles/tipover/model/TipOverModel.java
+++ src/puzzles/tipover/model/TipOverModel.java
@@ -1,14 +1,17 @@
 package puzzles.tipover.model;
 
+import solver.Configuration;
 import solver.Solver;
 import util.Observer;
+import util.Coordinates;
 
 import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
 /**
- * DESCRIPTION
+ * The Model used to keep track of the data for this puzzle, and all behaviors necessary for interacting
+ * with any type of user-interface.
  * @author Damon Gonzalez
  * November 2021
  */
@@ -22,20 +25,14 @@ public class TipOverModel {
     /** A list of observers to this model */
     private final List<Observer<TipOverModel, Object>> observers = new LinkedList<>();
 
-    enum Direction{//GET PROPER USAGE OF ENUMS FROM TOLL LAB
-        NORTH,
-        SOUTH,
-        EAST,
-        WEST
-    }
     /**
-     * Loads a new puzzle into this model using safeLoad() and given a filename, then notifies observers
+     * Loads a new puzzle into this model given a filename and using safeLoad(), then notifies observers
      * @param filename The file of the initial configuration of the puzzle
      */
     public void load(String filename){
         safeLoad(filename);
         steps = 0;
-        notifyObservers();
+        notifyObservers("");
     }
 
     /**
@@ -59,42 +56,90 @@ public class TipOverModel {
         }
     }
 
+    /**
+     * Used to cheat and show the user the next step in the shortest path from the current Configuration to the solution. If
+     * there is no solution, then the user is not told and a valid move is chosen at random.
+     */
     public void cheat(){
-        setNextStep();
-        notifyObservers();
-    }
-
-    private void setNextStep(){
-        currentConfig = (TipOverConfig) Solver.BFS(currentConfig).get(1);
+        List<Configuration> path = Solver.BFS(currentConfig);
+        if(path.size() > 1) currentConfig = (TipOverConfig) Solver.BFS(currentConfig).get(1);
+        else currentConfig = (TipOverConfig) currentConfig.getNeighbors().get((int) (currentConfig.getNeighbors().size() * Math.random()));
         steps++;
+        notifyObservers("");
     }
 
+    /**
+     * Replaces the current Configuration with one moved by the tipper in a specified direction
+     * @param direction The direction given by the user
+     */
     public void move(String direction){
-        System.out.println("MOVE THE TIPPER " + direction);
-        steps++;
+        List<Configuration> neighbors = currentConfig.getNeighbors();
+        Coordinates requestedMove = getCoordinates(direction);
+        for(Configuration neighbor : neighbors){
+            Coordinates neighborTipperLocation = ((TipOverConfig) neighbor).getTipperLocation();
+            if (neighborTipperLocation.equals(requestedMove)) {
+                currentConfig = (TipOverConfig) neighbor;
+                steps++;
+                notifyObservers("");
+                return;
+            }
+        }
+        notifyObservers(direction);//IF THE DIRECTION WAS INVALID
     }
 
+    /**
+     * Private helper method used to help determine if a move requested by the user can legally be performed
+     * @param direction The direction which the user wants the tipper to go, either NORTH, SOUTH, EAST, WEST
+     * @return A Coordinates object representing the location of the tipper if it were to move in the specified direction,
+     *  if no valid direction is given it returns a Coordinates object with values row: -1 and col: -1.
+     */
+    private Coordinates getCoordinates(String direction){
+        Coordinates tipperLocation = currentConfig.getTipperLocation();
+        switch (direction){
+            case "NORTH" -> {
+                return new Coordinates(tipperLocation.row() - 1, tipperLocation.col());
+            }
+            case "SOUTH" -> {
+                return new Coordinates(tipperLocation.row() + 1, tipperLocation.col());
+            }
+            case "WEST" -> {
+                return new Coordinates(tipperLocation.row(), tipperLocation.col() - 1);
+            }
+            case "EAST" -> {
+                return new Coordinates(tipperLocation.row(), tipperLocation.col() + 1);
+            }
+            default -> {
+                return new Coordinates(-1, -1);//BY DEFINITION INVALID COORDINATES IF NO PROPER DIRECTION IS GIVEN
+            }
+        }
+    }
+
+    /**
+     * Public getter for the current Configuration
+     * @return returns the TipOverConfig that represents the current
+     */
     public TipOverConfig getCurrentConfig() { return currentConfig; }
 
+    /**
+     * Public getter for steps
+     * @return An int value of the amount of steps the user has taken on this puzzle
+     */
     public int getNumSteps() { return steps; }
 
+    /**
+     * Adds an object of type Observer to the list of observers held in this model
+     * @param o The observer to be added
+     */
     public void addObserver(Observer o){
         observers.add(o);
     }
 
-    private void notifyObservers(){
-        for(Observer observer : observers)
-            observer.update(this, null);
-    }
-    /*
-     * Code here includes...
-     * Additional data variables for anything needed beyond what is in
-     *   the config object to describe the current state of the puzzle
-     * Methods to support the controller part of the GUI, e.g., load, move
-     * Methods and data to support the "subject" side of the Observer pattern
-     *
-     * WARNING: To support the hint command, you will likely have to do
-     *   a cast of Config to TipOverConfig somewhere, since the solve
-     *   method works with, and returns, objects of type Configuration.
+    /**
+     * Notifies all observers if there is a change within the model
+     * @param data Extra data given to the observers if necessary
      */
+    private void notifyObservers(String data){
+        for(Observer observer : observers)
+            observer.update(this, data);
+    }
 }
